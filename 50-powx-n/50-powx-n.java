552cc8a8827981e926f5b9cb53d0d756bd69576a
mouli <66820332+mouli7625@users.noreply.github.com>
2022-05-18 11:23:41 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (83.29%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,18 +1,9 @@
 import java.util.*;
 class Solution {
    static public double myPow(double x, int n) {
-       if(n<0)
-       {
-         
-           
+       
            return(Math.pow(x,n));
        }
-       else
-       {
-           return(Math.pow(x,n));
-       }
-        
-    }
     
     public static void main(String[]args)
     {
